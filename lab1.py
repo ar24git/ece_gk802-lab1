6eed98a359ced3beed5bcfda7e11e574f6c9b927
ar24git <arthurantonuyk@gmail.com>
2024-02-26 15:47:43 +0200

    cookies in more detail

--- lab1.py
+++ lab1.py
@@ -35,5 +35,13 @@ cookies = response.headers.get("Set-Cookie")
 
 if cookies:
     print(f"The cookies are {cookies}")
+
+    # Iterate over each cookie and print its name and expiry time
+    # Get cookie information from the response
+
+    cookies = response.cookies
+    for cookie in cookies:
+        print("Cookie Name:", cookie.name)
+        print("Expiry Time (UNIX timestamp):", cookie.expires)
 else:
     print("No cookies found")
